d30333c1f24d289b1ee8157bd6bf2c3500f7af59
vivcis <ceciliaorji.co@gmail.com>
2023-05-08 16:44:12 +0100

    add users table with unique and foreign key constraints

--- api/server.go
+++ api/server.go
@@ -21,7 +21,10 @@ func NewServer(store db.Store) *Server {
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterValidation("currency", validCurrency)
+		err := v.RegisterValidation("currency", validCurrency)
+		if err != nil {
+			return nil
+		}
 	}
 
 	router.POST("/accounts", server.createAccount)
